6f9d7131d2d43eea883caffad2e906d7597d2447
flymec <91526438+flymec@users.noreply.github.com>
2026-03-15 11:53:49 +0800

    Update javdayss.js

--- Widgets/javdayss.js
+++ Widgets/javdayss.js
@@ -5,7 +5,7 @@ var WidgetMetadata = {
   description: "获取 JAVDay 推荐",
   author: "flyme",
   site: "https://javday.app",
-  version: "1.5.0",
+  version: "1.1.3",
   requiredVersion: "0.0.1",
   detailCacheDuration: 60,
   modules: [
