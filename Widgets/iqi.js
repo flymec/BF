ebd3370b18dce25432a546d90a6144bb60231c49
flymec <91526438+flymec@users.noreply.github.com>
2026-03-21 19:44:00 +0800

    Update iqi.js

--- Widgets/iqi.js
+++ Widgets/iqi.js
@@ -1,9 +1,9 @@
 WidgetMetadata = {
   id: "forward.iqiyi",
-  title: "爱奇艺视频",
-  description: "爱奇艺视频搜索与热播推荐",
+  title: "视频",
+  description: "热播推荐",
   author: "Forward",
-  site: "https://www.iqiyi.com",
+  site: "https://www..com",
   version: "1.5.0",
   requiredVersion: "0.0.1",
   detailCacheDuration: 60,
